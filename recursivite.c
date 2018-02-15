acea920ba1731ee383a182743001ea49cb54f927
farahayar <36500903+farahayar@users.noreply.github.com>
2018-02-15 20:00:47 +0100

    Add files via upload

---
+++ recursivite.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int factorielle2(int n,int f)
+{
+   if(n==0||n==1)
+	{return f;}
+	else
+	{return factorielle2(n-1,n*f);
+	}
+}
+int main (void)
+{int f=1,n;
+	puts("donner n");
+	scanf("%d",&n);
+	f=factorielle2(n,f);
+	printf("%d!=%d\n",n,f);
+return 0;
+	}
+
