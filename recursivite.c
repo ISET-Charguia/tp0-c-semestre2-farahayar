a75d89d58c80af640c3839544626d706eb394dcc
farahayar <36500903+farahayar@users.noreply.github.com>
2018-02-15 20:02:23 +0100

    Update recursivite.c

--- recursivite.c
+++ recursivite.c
@@ -9,8 +9,11 @@ int factorielle2(int n,int f)
 }
 int main (void)
 {int f=1,n;
+        do
+	{
 	puts("donner n");
 	scanf("%d",&n);
+	}while(n<0);
 	f=factorielle2(n,f);
 	printf("%d!=%d\n",n,f);
 return 0;
